ff27fa014d7ce776c0108189ae6c585d702678d7
Qayoomitcourse <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-04 12:32:46 +0500

    Add files via upload

---
+++ Exercise01/Exercise01.ts
@@ -0,0 +1,4 @@
+// Question 2 : personnel message 
+
+let personName: string = "Qayoom";
+console.log(`hello ${personName} would you like to learn some typescript today?`)
---
+++ Exercise01/exercise01.js
@@ -0,0 +1,3 @@
+// Question 2 : personnel message 
+var personName = "Qayoom";
+console.log("hello ".concat(personName, " would you like to learn some typescript today?"));
