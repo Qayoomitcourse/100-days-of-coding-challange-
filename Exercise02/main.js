9b3f290b3e81f6ebb1829e9d0b7161b4a3645f13
Qayoomitcourse <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-04 12:42:26 +0500

    Add files via upload

---
+++ Exercise02/main.js
@@ -0,0 +1,7 @@
+var personName = "qayoom";
+//lowercase
+console.log(personName.toLowerCase());
+// uppercase
+console.log(personName.toUpperCase());
+// titlecase 
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1));
---
+++ Exercise02/main.ts
@@ -0,0 +1,7 @@
+let personName: string = "qayoom"
+//lowercase
+console.log(personName.toLowerCase());
+// uppercase
+console.log(personName.toUpperCase())
+// titlecase 
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1))
