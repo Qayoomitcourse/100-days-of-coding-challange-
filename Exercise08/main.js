304b5b71c6e1b2eb63ca52797f710ff7ce3890de
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-05 12:05:39 +0500

    Delete Exercise08 directory

--- Exercise08/main.js
+++
@@ -1,3 +0,0 @@
-// Question No. 3 / day 3 Challange
-var favorateNumber = 8;
-console.log("my favorate number is ".concat(favorateNumber));
--- Exercise08/main.ts
+++
@@ -1,6 +0,0 @@
-// Question No. 3 / day 3 Challange
-
-let favorateNumber: number = 8
-console.log (`my favorate number is ${favorateNumber}`)
-
-
