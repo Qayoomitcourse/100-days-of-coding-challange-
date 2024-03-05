0f670b211ff11ff9e58825e90ed8ea73a1814701
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-05 12:15:01 +0500

    Add files via upload
    Question No. 10 / Day 4

---
+++ Exercise09/main.js
@@ -0,0 +1,5 @@
+// Question No. 10 / day 4 Challange
+// Qayoom, 2024-03-06
+// This programm prints a personnal message 
+var myName = "Qayoom";
+console.log("Hello ".concat(myName, " would you like to learn some typescript today?"));
---
+++ Exercise09/main.ts
@@ -0,0 +1,8 @@
+// Question No. 10 / day 4 Challange
+
+// Qayoom, 2024-03-06
+// This programm prints a personnal message 
+
+let myName: string = "Qayoom"
+console.log(`Hello ${myName} would you like to learn some typescript today?`)
+
