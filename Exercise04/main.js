08ba4a8b99d96715ecce1db311706814c518eb7d
Qayoomitcourse <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-05 11:22:18 +0500

    Add files via upload

---
+++ Exercise04/main.js
@@ -0,0 +1,4 @@
+// Question No. 4
+var famourPerson = "Albert Einsten";
+var quote = "".concat(famourPerson, " once said We cannot solve our problems with the same thinking we used when we created them");
+console.log(quote);
---
+++ Exercise04/main.ts
@@ -0,0 +1,6 @@
+// Question No. 4
+
+let famourPerson: string = "Albert Einsten"
+let quote: string = `${famourPerson} once said We cannot solve our problems with the same thinking we used when we created them`
+
+console.log(quote)
