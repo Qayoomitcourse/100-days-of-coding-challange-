e22fbe6c880824ed24d40f1588a3cc140640f6b1
Qayoomitcourse <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-05 11:38:58 +0500

    Add files via upload

---
+++ Exercise05/main.js
@@ -0,0 +1,4 @@
+// Question No. 5 / day 2 Challan
+var Name = "\t\n Qayoom \t\n";
+console.log(Name);
+console.log(Name.trim());
---
+++ Exercise05/main.ts
@@ -0,0 +1,5 @@
+// Question No. 5 / day 2 Challan
+
+let Name: string = "\t\n Qayoom \t\n"
+console.log(Name)
+console.log(Name.trim())
