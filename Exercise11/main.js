3985f98f4e4d452f943e9385f7dddc30f5daaf59
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-05 14:51:53 +0500

    Add files via upload

---
+++ Exercise11/main.js
@@ -0,0 +1,5 @@
+// Question No. 11 / day 4 Challange
+var names = ["Asif", "Sajid", "Sami"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Exercise11/main.ts
@@ -0,0 +1,8 @@
+// Question No. 11 / day 4 Challange
+
+
+let names: string[] = ["Asif", "Sajid", "Sami"]
+for (let i = 0; i <names.length; i++) {
+        console.log(names[i]);
+}
+
