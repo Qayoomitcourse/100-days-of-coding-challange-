543d097b7d5154cdd01246ccc87a6fe34ff8cf85
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-07 11:39:05 +0500

    Add files via upload

---
+++ Exercise13/main.js
@@ -0,0 +1,9 @@
+var myTransport = ["Car", "Jeep", "Motorcycle", "Bus", "Aeroplane"];
+myTransport.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
+/*
+myTransport.forEach(transport => {
+        console.log(`I would like to own a ${transport}.`);
+    });
+    */ 
---
+++ Exercise13/main.ts
@@ -0,0 +1,7 @@
+
+let myTransport: string [] = ["Car", "Jeep", "Motorcycle", "Bus", "Aeroplane" ];
+
+myTransport.forEach(transport => {
+
+        console.log(`I would like to own a ${transport}.`)
+})
