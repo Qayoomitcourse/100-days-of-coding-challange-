0d37d67bcc45711edd80a00e8bbb21197b3be7c9
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-03-09 16:53:42 +0500

    Add files via upload

---
+++ Exercise14/main.js
@@ -0,0 +1,4 @@
+var guests = ["Munawar", "Asif", "Sajid", "Ayaz"];
+guests.forEach(function (guest) {
+    console.log("Dear Mr. ".concat(guest, ", you are cardialy invited to join us for the dinner!"));
+});
---
+++ Exercise14/main.ts
@@ -0,0 +1,7 @@
+// *Question 14:** Guest List:
+
+let guests : string [] = ["Munawar", "Asif", "Sajid", "Ayaz"]
+
+guests.forEach (guest =>{
+    console.log(`Dear Mr. ${guest}, you are cardialy invited to join us for the dinner!`)
+})
