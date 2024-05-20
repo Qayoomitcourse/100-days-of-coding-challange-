7ab31da363558c69916a47198d2b41c08c345b9a
Abdul Qayoom <162087002+Qayoomitcourse@users.noreply.github.com>
2024-05-20 19:16:42 +0500

    I have prepared list of countries as suggested in Question 20

---
+++ Exercise20/main.js
@@ -0,0 +1,13 @@
+// // Define an array of countries
+// let countries: string[] = ["USA", "Canada", "France", "Japan", "Australia"];
+// // Assign the list of countries to another variable
+// let listofCountries = countries;
+// // Print the list of countries
+// console.log(countries);
+// console.log(listofCountries);
+var countries = ["USA", "Canada", "France", "Japan", "Australia"];
+console.log("List of Countries:");
+for (var _i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
+    var country = countries_1[_i];
+    console.log(country);
+}
---
+++ Exercise20/main.ts
@@ -0,0 +1,9 @@
+
+let countries: string[] = ["USA", "Canada", "France", "Japan", "Australia"];
+
+console.log("List of Countries:");
+
+for (let country of countries) {
+    console.log(country)
+}
+
